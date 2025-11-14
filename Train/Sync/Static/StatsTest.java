86d8d372f1e2ef2e29844ef8631f31d7eb0289a9
Pranav AK <159046042+ipranavak@users.noreply.github.com>
2025-11-14 23:20:09 +0530

    Create StatsTest.java

---
+++ Train/Sync/Static/StatsTest.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class HospitalStats implements Serializable {
+    static int patientCount = 0;
+
+    public static synchronized void saveStats(){
+        try(ObjectOutputStream o =
+            new ObjectOutputStream(new FileOutputStream("stats.ser"))){
+            o.writeObject(patientCount);
+            System.out.println("Saved count: "+patientCount);
+        } catch(Exception e){}
+    }
+}
+
+public class StatsTest {
+    public static void main(String[] args) {
+        Runnable r = () -> {
+            HospitalStats.patientCount++;
+            HospitalStats.saveStats();
+        };
+        new Thread(r).start();
+        new Thread(r).start();
+    }
+}
